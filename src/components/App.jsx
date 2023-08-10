3feaed6ef5e45893c418b94e7bede01e0f16608a
Iryna220 <ibiriszka951@gmail.com>
2023-08-10 21:15:22 +0200

    update hw 2 feedback
    mark up feedback part

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Section from './Section';
+import Statistics from './Statistics';
+import Notification from './Notification';
 
 class App extends Component {
   state = {
@@ -8,6 +10,22 @@ class App extends Component {
     neutral: 0,
     bad: 0,
   };
+  onLeaveFeedback = state => {
+    this.setState(prevState => {
+      return { [state]: prevState[state] + 1 };
+    });
+  };
+
+  countTotalFeedback() {
+    const { good, neutral, bad } = this.state;
+    return good + neutral + bad;
+  }
+
+  countPositiveFeedbackPercentage() {
+    const { good } = this.state;
+    const totalFeedback = this.countTotalFeedback();
+    return Math.round((good / totalFeedback) * 100);
+  }
 
   render() {
     const { good, neutral, bad } = this.state;
@@ -21,7 +39,19 @@ class App extends Component {
             onLeaveFeedback={this.onLeaveFeedback}
           />
         </Section>
-        <Section title="Statistics"></Section>
+        <Section title="Statistics">
+          {this.countTotalFeedback() > 0 ? (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          ) : (
+            <Notification message="There is no feedback" />
+          )}
+        </Section>
       </div>
     );
   }
